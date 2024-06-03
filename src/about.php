7cc2912f38c6155aab78b9940e8d2e9726414538
CPU_Governor <cpu.governor@gmail.com>
2024-06-03 22:14:01 +0100

    initia

---
+++ src/about.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+     <head>
+     	<title>About.php</title>
+     </head>
+     <body>
+     	<h1>About</h1>
+    <p>A  site built to help me learn php and Mysql (mariaDb).</p>
+          <h1>Contact</h1>
+          <p>Email: <a href="#">cpu.governor@gmail.com</a><br>
+          	Github: <a href="#">github.com/gamewiki</a>
+          </p>
+     </body>
+</html>
---
+++ src/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+   <head>
+   	<title>Game wiki</title>
+   </head>
+   <body>
+   	<nav>
+   	    <ul>
+   	       <li><a href="#">Home</a></li>
+              <li><a href="#">Browse Games</a></li>
+              <li><a href="About.php">About</a></li>
+   	   </ul>
+   	</nav>
+   <h1>Game wiki</h2>
+   <p>A  php based site with Mysql (mariaDb) that </p>
+   <div class="outline">
+   </body>
+</html>
