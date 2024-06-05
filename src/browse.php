bb2e9e0a0a77fcc223472e18d66341fa45b84721
Okuchukwu Nzeogwu <cpu.governor@gmail.com>
2024-06-05 01:36:34 +0100

    Add files via upload

---
+++ src/browse.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Browse<title>
+	</head>
+	<body>
+		<nav>
+   	    <ul>
+   	       <li><a href="#">Home</a></li>
+              <li><a href="#">Browse Games</a></li>
+              <li><a href="About.php">About</a></li>
+   	   </ul>
+   	</nav>
+   
+	</body>
+</html>
